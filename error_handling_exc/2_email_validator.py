dbc20d1e6572e74d78e3cb3f2fcc9674948ff104
zhmarinov <j.marinovmt@gmail.com>
2023-06-02 20:15:12 +0300

    Create 2_email_validator.py

---
+++ error_handling_exc/2_email_validator.py
@@ -0,0 +1,47 @@
+class NameTooShortError(Exception):
+    pass
+
+
+class MustContainAtSymbolError(Exception):
+    pass
+
+
+class InvalidDomainError(Exception):
+    pass
+
+
+class MoreThenOneAtSymbolError(Exception):
+    pass
+
+
+class MoreThenOneDotError(Exception):
+    pass
+
+
+VALID_DOMAINS = ['com', 'bg', 'org', 'net']
+
+while True:
+    emails_line = input()
+    if emails_line == 'End':
+        break
+
+    list_of_email = emails_line.split('@')
+
+    email_name = list_of_email[0]
+    if len(email_name) < 4:
+        raise NameTooShortError("Name must be more than 4 characters")
+
+    if '@' not in emails_line:
+        raise MustContainAtSymbolError("Email must contain @")
+
+    domain_name = emails_line.split('.')
+    if domain_name[-1] not in VALID_DOMAINS:
+        raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
+
+    if emails_line.count('@') > 1:
+        raise MoreThenOneAtSymbolError("Email should contain only one @ symbol")
+
+    if emails_line.count('.') > 1:
+        raise MoreThenOneAtSymbolError("Email should contain only one '.' symbol")
+
+    print("Email is valid")
